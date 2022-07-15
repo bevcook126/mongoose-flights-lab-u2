223a488e1b3e6785e88d4a2c62c7b9def8352c94
username <bevcook126@gmail.com>
2022-07-15 16:01:58 -0700

    ascending flights

--- controllers/flights.js
+++ controllers/flights.js
@@ -18,11 +18,19 @@ function create(req, res) {
 }
 
 function newFlight(req, res) {
-    res.render('flights/new');
+    const newFlight = new Flight();
+    const dt = newFlight.departs;
+    let departsDate = `${dt.getFullYear()}-${(dt.getMonth() + 1).toString().padStart(2, '0')}`;
+    departsDate += `-${dt.getDate().toString().padStart(2, '0')}T${dt.toTimeString().slice(0, 5)}`;
+    res.render('flights/new', { departsDate });    
 }
 
 function index(req, res) {
     Flight.find({}, function(err, flights) {
+        flights.sort((a,b) => Number(a.departs) - Number(b.departs));
         res.render('flights/index', { flights });
-    }); 
+    });
+    // if (departs > flight.departs){
+    //     let flight = '<p style="color: red;">'
+    // } 
 }
